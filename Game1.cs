a2fec2518b4b9fc22467ee253abbc2bf26108f89
Jonathan Gaboury <362568651@GoTVDSB.ca>
2024-12-04 09:38:17 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace topic_2_monogame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D circleTexture, rectangleTexture;
+        Rectangle circlerect;
+
+        SpriteFont textFont;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            circlerect = new Rectangle(300, 100, 100, 50);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            circleTexture = Content.Load<Texture2D>("circle");
+            rectangleTexture = Content.Load<Texture2D>("rectangle");
+            textFont = Content.Load<SpriteFont>("TextFont");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(rectangleTexture, new Rectangle(300,100,100,50), Color.Orange);
+
+            _spriteBatch.Draw(circleTexture, circlerect, Color.Brown);
+
+            _spriteBatch.DrawString(textFont, "rAinE", new Vector2(250,250), Color.Indigo);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
